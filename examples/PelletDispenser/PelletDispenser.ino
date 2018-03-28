54dfa3148c897a30fa5201ccde4a2b8d4af84b0f
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-28 17:05:29 -0400

    Removed unnecessary header includes from ino files.

--- examples/PelletDispenser/PelletDispenser.ino
+++ examples/PelletDispenser/PelletDispenser.ino
@@ -1,34 +1,3 @@
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
-#include <ArduinoJson.h>
-#include <JsonSanitizer.h>
-#include <JsonStream.h>
-#include <Array.h>
-#include <Vector.h>
-#include <MemoryFree.h>
-#include <ConstantVariable.h>
-#include <SavedVariable.h>
-#include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
-#include <SPI.h>
-#include <TMC429.h>
-#include <TMC2130.h>
-#include <EventController.h>
-
-#include <ModularClient.h>
-#include <HBridgeController.h>
-#include <OpticalSwitchInterface.h>
-#include <AudioController.h>
-
-#include <ModularServer.h>
-#include <ModularDeviceBase.h>
-#include <StepDirController.h>
-#include <StepperController.h>
-#include <StageController.h>
 #include <PelletDispenser.h>
 
 
