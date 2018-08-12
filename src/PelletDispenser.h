4628ab7e885b65de6c4dd4898ad37d62862f705a
Peter Polidoro <peterpolidoro@gmail.com>
2018-08-12 12:54:07 -0400

    polidorop@janelia.hhmi.org -> peterpolidoro@gmail.com

--- src/PelletDispenser.h
+++ src/PelletDispenser.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro polidorop@janelia.hhmi.org
+// Peter Polidoro peterpolidoro@gmail.com
 // ----------------------------------------------------------------------------
 #ifndef PELLET_DISPENSER_H
 #define PELLET_DISPENSER_H
--- src/PelletDispenser/Constants.h
+++ src/PelletDispenser/Constants.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro polidorop@janelia.hhmi.org
+// Peter Polidoro peterpolidoro@gmail.com
 // ----------------------------------------------------------------------------
 #ifndef PELLET_DISPENSER_CONSTANTS_H
 #define PELLET_DISPENSER_CONSTANTS_H
